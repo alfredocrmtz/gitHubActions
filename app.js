52726ad39aa8718cdb827ad5a0887f23234c01b3
alfredo.cruz <alfredo.cruz@becasbenitojuarez.gob.mx>
2024-03-13 19:30:40 -0600

    Inicio

---
+++ app.js
@@ -0,0 +1,4 @@
+const moment = require('moment');
+console.log('HELL, USING GITHUB ACTIONS AS A DEMO');
+const date = moment().format('MMM Do YYYY, h:mm:ss')
+console.log(`Current Date and Time is ${date}`);
